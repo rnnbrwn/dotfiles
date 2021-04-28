45d1e52d19f4e751f9dcce6733751f6000dd12d0
Ronnie Brown <91823+rnnbrwn@users.noreply.github.com>
2021-04-28 21:54:33 +0100

    build: homebrew setup added

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -6,18 +6,31 @@ echo "\n<<< Starting Homebrew Setup >>>\n"
 brew install httpie
 brew install bat
 
-brew install google-chrome-canary
-brew install google-chrome
-brew install firefox
-brew install firefox-nightly
-brew install firefox-developer-edition
-brew install microsoft-edge
-brew install microsoft-edge-dev
+
+echo "\n<<< Downloading Browsers >>>\n"
+
+brew install google-chrome-canary --no-quarantine
+brew install google-chrome --no-quarantine
+brew install firefox --no-quarantine
+brew install firefox-nightly --no-quarantine
+brew install firefox-developer-edition --no-quarantine
+brew install microsoft-edge --no-quarantine
+brew install microsoft-edge-dev --no-quarantine
+
+echo "\n<<< Downloading Design Tools >>>\n"
+
 brew install affinity-designer-beta
+brew install figma
+
+echo "\n<<< Downloading Development Tools >>>\n"
+
 brew install visual-studio-code
 brew install visual-studio-code-insiders
+
+echo "\n<<< Downloading Miscellaneous Tools >>>\n"
+
 brew install spotify
 brew install dropbox
 brew install dropbox-passwords
 brew install tunnelbear
-brew install figma
+brew install --no-quarantine alfred
